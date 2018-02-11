231b7a8871896efa64ac542013a6f1dfb19265db
zenglenn42 <gsnospampls@pobox.com>
2018-02-11 08:18:41 -0600

    rollback abstraction to straight SDL code since former is not working

--- opengl-tutorial-tbb/main.cpp
+++ opengl-tutorial-tbb/main.cpp
@@ -2,7 +2,6 @@
 //  main.cpp
 //  opengl-tutorial-tbb
 //
-//  Created by m on 2/10/18.
 //  Copyright © 2018 zenglenn42. All rights reserved.
 //
 
@@ -10,6 +9,28 @@
 #include "display.h"
 
 int main(int argc, const char * argv[]) {
-    Display display(800, 600, "Hello World");
+    //Display display(800, 600, "Hello World");
+
+    bool quit = false;
+    SDL_Event event;
+    
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    SDL_Window * screen = SDL_CreateWindow("My SDL Empty Window",
+                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+        
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+    }
+    
+    SDL_Quit();
     return 0;
 }
